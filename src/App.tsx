a1ae56983829207c3b775b635a5dcceae96308da
ashish <ashishmangla1991@gmail.com>
2021-05-26 09:37:38 +0530

    add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 function App() {
   return (
     <>
-
+<Header/>
       <Footer/>
     </>
   );
