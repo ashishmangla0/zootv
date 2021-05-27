8af1132feb9c674cac5f1442732af1ef2e7df0c2
ashish <ashishmangla1991@gmail.com>
2021-05-27 14:10:25 +0530

    add new componems

---
+++ src/Home.tsx
@@ -0,0 +1,40 @@
+import Hero from "./Components/Hero";
+import MovieList from "./Components/MovieList";
+import Container from "./Elements/Container";
+import requests from './requests';
+const Home = () => {
+    return (
+        <>
+        <Hero>
+            wrwerwerwe rwe
+            </Hero>
+            <section>
+                <Container>
+                    <MovieList title="Up Coming" fetchUrl={requests.upcoming} />
+                </Container>
+            </section>
+            <section>
+                <Container>
+                    <MovieList title="Top Rated" fetchUrl={requests.topRated} />
+                </Container>
+            </section>
+            <section>
+                <Container>
+                    <MovieList title="Trending" fetchUrl={requests.fetchTrending} />
+                </Container>
+            </section>
+            <section>
+                <Container>
+                    <MovieList title="Trending" fetchUrl={requests.fetchTrending} />
+                </Container>
+            </section>
+            <section>
+                <Container>
+                    <MovieList title="Trending" fetchUrl={requests.fetchTrending} />
+                </Container>
+            </section>
+        </>
+
+    )
+}
+export default Home
