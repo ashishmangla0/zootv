7a87e518d71b6ec54107f17c676b17c4c6069199
ashish <ashishmangla1991@gmail.com>
2021-05-27 16:55:50 +0530

    a

--- src/Pages/Terms/index.jsx
+++ src/Pages/Terms/index.jsx
@@ -9,9 +9,9 @@ const Terms = () => {
                 <Heading level={1} title="Terms and Conditions" className={"block__title block__title--inner"} />
                 <Breadcrumb />
             </Hero>
-            <section className="">
+            <section className="block">
                 <Container>
-                    <div>
+                    <div className="block__content">
                         <Heading level={2} title={`Terms and Conditions`} />
                         <p>Welcome to zootv!</p>
                         <p>
@@ -24,7 +24,7 @@ const Terms = () => {
                             The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.
                         </p>
                     </div>
-                    <div>
+                    <div className="block__content">
                         <Heading level={2} title={`Cookies`} />
                         <p>
                             We employ the use of cookies. By accessing zootv, you agreed to use cookies in agreement with the zootv's Privacy Policy.
@@ -33,7 +33,7 @@ const Terms = () => {
                             Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
                         </p>
                     </div>
-                    <div>
+                    <div className="block__content">
                         <Heading level={2} title={`License`} />
                         <p>
                             Unless otherwise stated, zootv and/or its licensors own the intellectual property rights for all material on zootv. All intellectual property rights are reserved. You may access this from zootv for your own personal use subjected to restrictions set in these terms and conditions.
