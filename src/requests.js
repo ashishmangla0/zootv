2beeb537abdf15c1953ccc8108dedd89756c5264
ashish <ashishmangla1991@gmail.com>
2021-05-27 14:10:58 +0530

    add new componems

--- src/requests.js
+++ src/requests.js
@@ -1,5 +1,7 @@
 const request = {
-    fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_API}`
+    fetchTrending: `trending/all/week?api_key=${process.env.REACT_APP_API}`,
+    topRated: `movie/top_rated?api_key=${process.env.REACT_APP_API}`,
+    upcoming: `movie/upcoming?api_key=${process.env.REACT_APP_API}`
 }
 
 export default request
