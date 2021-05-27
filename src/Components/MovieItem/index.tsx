491b6f07d862ee16dabef645a13f6d2d23fc2ad4
ashish <ashishmangla1991@gmail.com>
2021-05-27 07:14:11 +0530

    add new listing

--- src/Components/MovieItem/index.tsx
+++ src/Components/MovieItem/index.tsx
@@ -1,13 +1,18 @@
-type propsMovieItem ={
-    title:string,
-    className ?: string
+import Heading from "../../Elements/Heading"
+
+type propsMovieItem = {
+    title : string,
+    className ?: string,
+    imgpath: string
+
 }
-const MovieItem : React.FC<propsMovieItem> = ({title,className}) =>{
-return(
-    <article className={className ? className :''}>
-{title}
-    </article>
-)
+const MovieItem: React.FC<propsMovieItem> = ({ title, className,imgpath }) => {
+    return (
+        <article className={className ? className : ''}>
+            <Heading level={3} title={title} className={"asd"} />
+            <img src={imgpath} />
+        </article>
+    )
 }
 
 
